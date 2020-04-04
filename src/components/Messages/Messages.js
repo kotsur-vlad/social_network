c1f5b0c8c2bbdd4ff9322c45eb07784f9b61fe13
kotsur-vlad <kotsur.vlad@gmail.com>
2020-04-04 23:42:47 +0300

    add Routes to <Messages, Profile> components denotation Messages component

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -4,7 +4,20 @@ import styles from "./Messages.module.css"
 const Messages = () => {
 	return (
 		<div className={styles.messages}>
-			Dialogs
+			<div className={styles.dialog}>
+				Dialog with messages
+				<div className={styles.dialogItem}>Hi</div>
+				<div className={styles.dialogItem}>Hello</div>
+				<div className={styles.dialogItem}>How are u doing?</div>
+				<div className={styles.dialogItem}>Ty, nice</div>
+			</div>
+			<div className={styles.dialogList}>
+				List of dialogs with users
+				<div className={styles.dialogUserItem + ' ' + styles.active}>Kim</div>
+				<div className={styles.dialogUserItem}>Tim</div>
+				<div className={styles.dialogUserItem}>Vasya</div>
+				<div className={styles.dialogUserItem}>Putin</div>
+			</div>
 		</div>
 	);
 };
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import styles from './Navbar.module.css'
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
-			<ul>
-				<li><a href="/profile">Profile</a></li>
-				<li><a href="/messages">Messages</a></li>
-				<li><a href="#">News</a></li>
-				<li><a href="#">Music</a></li>
-				<li><a href="#">Settings</a></li>
+			<ul className={styles.list}>
+				<li className={styles.item}><NavLink to="/profile" activeClassName={styles.activeLink}>Profile</NavLink></li>
+				<li className={styles.item}><NavLink to="/messages" activeClassName={styles.activeLink}>Messages</NavLink></li>
+				<li className={styles.item}><a href="#">News</a></li>
+				<li className={styles.item}><a href="#">Music</a></li>
+				<li className={styles.item}><a href="#">Settings</a></li>
 			</ul>
 		</nav>
 	);
