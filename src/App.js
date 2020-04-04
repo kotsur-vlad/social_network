7f3d71e87eb0ebd82c070b506cc13ada9d8065ed
kotsur-vlad <kotsur.vlad@gmail.com>
2020-04-04 18:44:01 +0300

    add some Routes for test, using "react-router-dom" module

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Messages from "./components/Messages/Messages";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = () => {
 	return (
-		<div className="app-wrapper">
-			<Header/>
-			<Navbar/>
-			<div className="app-wrapper-content">
-				{/*<Profile/>*/}
-				<Messages/>
+		<BrowserRouter>
+			<div className="app-wrapper">
+				<Header/>
+				<Navbar/>
+				<div className="app-wrapper-content">
+					<Route path={"/profile"} component={Profile}/>
+					<Route path={"/messages"} component={Messages}/>
+				</div>
 			</div>
-		</div>
+		</BrowserRouter>
 	);
 };
 
