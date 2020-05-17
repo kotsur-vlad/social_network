bd7fa46a6aeb03b2bd0988eaac6ec111b460d281
kotsur-vlad <kotsur.vlad@gmail.com>
2020-05-17 20:14:15 +0300

    Used container components for "Messages" and "Profile". Refs replaced by events

--- src/index.js
+++ src/index.js
@@ -7,23 +7,20 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from "./redux/store-redux";
 
-let rerenderEntireTree = (state) => {
+let rerenderEntireTree = () => {
 	ReactDOM.render(
 		<React.StrictMode>
 			<BrowserRouter>
-				<App state={state} dispatch={store.dispatch.bind(store)} store={store} />
+				<App store={store}/>
 			</BrowserRouter>
 		</React.StrictMode>,
 		document.getElementById('root')
 	);
 };
 
-rerenderEntireTree(store.getState());
+rerenderEntireTree();
 
-store.subscribe(() => {
-	let state = store.getState();
-	rerenderEntireTree(state);
-});
+store.subscribe(rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
